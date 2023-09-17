36657f3dd50729129c2c433c173982d4e0673eb7
ItsAryanSharma <123888689+ItsAryanSharma@users.noreply.github.com>
2023-09-17 12:37:37 +0530

    Second Release

---
+++ P_2.js
@@ -0,0 +1,54 @@
+// Active Navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    }
+    else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+//  Nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+    a.addEventListener("click", function (event) {
+        if (event.target.classList.contains("dropdown-toggle")) {
+            return; // Prevent hiding the menu for dropdown toggle clicks
+        }
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 220, 2221, 3400);
+    counter("count2", 100, 70, 3400);
+    counter("count3", 110, 50, 3400);
+    counter("count4", 120, 2211, 3400);
+});
+
+// Must-Try Arrows
+document.getElementById('next').onclick = function () {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function () {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
